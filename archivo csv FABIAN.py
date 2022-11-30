2fa781bd4e84fbe58031458e8f72677c12beb79e
fabiancarvajal21 <113704882+fabiancarvajal21@users.noreply.github.com>
2022-11-29 19:52:04 -0500

    Add files via upload

---
+++ archivo csv FABIAN.py
@@ -0,0 +1,107 @@
+#FABIAN CARVAJAL AREVALO 
+
+import csv
+
+def creacion_matrix (array):
+    matrix = []
+    lengh = int(input("cuantos usuarios desea registrar: "))
+            
+    for i in range (lengh):
+        array.append ([])
+        for j in range (3):
+            date = str(input("Digite el "+array[i][j]+" de la persona: "))
+            array[i+1].append (date)
+        matrix.append (array)
+    return (matrix)            
+
+def creando(array, rout):
+    with open(rout,'w',newline='')as csvfile:
+        writer=csv.writer(csvfile, delimiter=';')
+        writer.writerows(array)
+
+def leyendo(dire):
+    with open(dire,'r',newline='')as csvfile:
+        reader=csv.reader(csvfile,delimiter=';')
+        for row in reader:
+            print(row) 
+            
+def agregando(dire, creacion):
+    with open(dire,'r',newline='')as csvfile:
+        reader=csv.reader(csvfile,delimiter=';')
+        data=[line for line in reader]
+    with open(dire,'w',newline='')as csvfile:
+        writer=csv.writer(csvfile,delimiter=';')
+        writer.writerows(data)
+        writer.writerows(creacion)            
+
+def eliminando(search):
+    dire=str("C:\Users\FABIAN CARVAJAL\Documents\Nuevo_archivo.csv")
+    with open(dire,'r',newline='')as csvfile:
+        reader=csv.reader(csvfile,delimiter=';')
+        data=[line for line in reader]
+    with open(dire,'w',newline='')as csvfile:
+        writer=csv.writer(csvfile,delimiter=';')
+        data.pop(search)
+        writer.writerows(data)
+    
+        
+                    
+            
+
+if __name__=="__main__":
+    
+    array = [['CODIGO', 'NOMBRE', 'APELLIDO']]        
+    creation = 0
+    
+    print("\n")
+    menu = False
+    
+    while (menu == False):
+    
+        print("""
+            1) CREAR UN ARCHIVO CSV.
+            2) LECTURA DE UN ARCHIVO CSV EXISTENTE.
+            3) ACTUALIZAR UN ARCHIVO CSV EXISTENTE.
+            4) ELIMINAR UN DATO DE UN ARCHIVO CSV.
+            5) SALIR DEL MENU DE OPCIONES 
+            """)
+        option = int(input("OPCION ELEGIDA: "))
+        if (option >= 1 and option <= 5):
+            if (option == 1):
+                print ("\n")
+                rout = str(input("Ingrese la ruta donde deseas guardar el archivo: "))
+                creacion_matrix (array)
+                creation = creando(array, rout)
+                print("\n")
+            elif (option == 2):
+                rout = str(input("Ingrese la ruta  donde guardaste el archivo: "))
+                reading = leyendo (rout)
+                print("\n")
+            elif (option == 3):
+                print ("""
+                    1) AGREGAR
+                    2) ELIMINAR
+                    """)
+                op = int(input("OPCION ELEGIDA: "))
+                if (op >= 1 and op <= 2):
+                    if (op == 1):
+                        ruta = str(input("Digita la ruta de la ubicacion del archivo: "))
+                        array_2 = [['263646','pedro','lopez'],['22334455',['julio','caceres']]]
+                        add = agregando (ruta, array_2)
+                        print ("\n")
+                    elif (op == 2):
+                        element = int(input(" Digita la Posicion del elemento que deseas eliminar []: "))
+                        rout = str(input("Dame la ruta: "))
+                        pop = eliminando (element, rout)
+                        print("\n")
+            elif (option == 4):
+                element = int(input("Digita la Posicion del elemento que desea eliminar []: "))                    
+                pop = eliminando (element)
+                print("\n")
+            elif (option == 5):
+                print ("\n")
+                print ("---HAS SALIDO DEL MENU DE OPCIONES ---")    
+                menu = True            
+                    
+                
+    
